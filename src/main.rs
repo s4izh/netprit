a2a4cd115eb1a3464cf73ed3d3cf5dc3be813695
s4izh <sergio.sanz.martinez@proton.me>
2024-08-26 23:25:44 +0200

    rust: started rust version

--- src/main.rs
+++ src/main.rs
@@ -2,36 +2,69 @@ use std::{
     fmt::format,
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    sync::Arc, sync::Mutex
 };
+
+use std::process::Command;
+use std::thread;
 
 const CONTROL_PORT: u32 = 5000;
 const WORKER_PORT: u32 = 5001;
 
-fn control_thread_loop(port: u32) -> std::io::Result<()> {
+fn control_thread_loop(port: u32, worker_pid: Arc<Mutex<i32>>) -> std::io::Result<()> {
     let address = format!("0.0.0.0:{}", port);
     let listener = TcpListener::bind(address)?;
 
-    let mut buf = Vec::with_capacity(128);
+    println!("Control listening on port {port}...");
 
     for stream in listener.incoming() {
         if let Ok(mut stream) = stream {
-            let _ = stream.write_all(b"testing\n");
-            stream.read_to_end(&mut buf)?;
-            for i in &buf {
-                print!("{}", i);
-            }
+            let pid = worker_pid.lock().unwrap();
+            let response = format!("testing, worker_pid {}\n", pid);
+            let _ = stream.write_all(response.as_str().as_bytes());
+        }
+    } 
+    Ok(())
+}
+ 
+fn worker_thread_loop(port: u32, worker_pid: Arc<Mutex<i32>>) -> std::io::Result<()> {
+    let address = format!("0.0.0.0:{}", port);
+    let listener = TcpListener::bind(address)?;
+
+    println!("Worker listening on port {port}...");
+ 
+    for stream in listener.incoming() {
+        if let Ok(mut stream) = stream {
+            Command::new("ls")
+                .spawn()
+                .expect("sh command failed to start");
+
+            *worker_pid.lock().unwrap() = 1000;
         }
     }
     Ok(())
 }
+ 
+fn main() -> () {
+    let worker_pid: Arc<Mutex<i32>> = Arc::new(Mutex::new(-1));
+    let worker_pid_clone: Arc<Mutex<i32>> = Arc::clone(&worker_pid);
 
-fn main() {
-    println!("Hello, world!");
-
-    std::thread::spawn(|| {
-        let _ = control_thread_loop(CONTROL_PORT);
+    let control_handle = thread::spawn(move || {
+        if let Err(e) = control_thread_loop(CONTROL_PORT, worker_pid_clone) {
+            eprintln!("Error in control thread: {:?}", e);
+        }
     });
 
-    let dur = std::time::Duration::from_secs(1000);
-    std::thread::sleep(dur);
+    let worker_handle = thread::spawn(move || {
+        if let Err(e) = worker_thread_loop(WORKER_PORT, worker_pid) {
+            eprintln!("Error in control thread: {:?}", e);
+        }
+    });
+
+    control_handle.join().expect("Control thread panicked");
+    worker_handle.join().expect("Worker thread panicked");
+
+    loop {
+        thread::park();
+    }
 }
